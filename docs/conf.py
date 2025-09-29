fd3c2fe0d319d64ca5aab5d2f0f3c9db5d161b19
Justus Magin <keewis@posteo.de>
2025-09-29 16:30:14 +0300

    configure myst to use `colon_fence` syntax

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,11 @@ templates_path = ["_templates"]
 exclude_patterns = ["_build", "directory"]
 
 
+# -- Myst configuration ------------------------------------------------------
+
+myst_enable_extensions = ["colon_fence"]
+
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
